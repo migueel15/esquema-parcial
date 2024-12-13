04ebbad13d37f333edafc3ddb432d406d09dfb90
migueel15 <miguelangeldorado10@gmail.com>
2024-12-13 15:58:52 +0100

    url actualizadas

--- frontend/src/app/services/auth.config.ts
+++ frontend/src/app/services/auth.config.ts
@@ -6,6 +6,7 @@ export const authConfig: AuthConfig = {
   strictDiscoveryDocumentValidation: false,
   showDebugInformation: true,
   clientId: environment.CLIENT_ID,
-  redirectUri: window.location.origin + '/dashboard',
+  redirectUri: 'https://frontend-parcial.vercel.app/dashboard',
+  // redirectUri: window.location.origin + '/dashboard',
   scope: 'openid profile email',
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   CLIENT_ID:
-    '279955462362-vslr6hsb46n5n67keoctg43i0kibn7b1.apps.googleusercontent.com',
-  BACKEND_URL: 'http://localhost:8000',
+    '279955462362-t9bd5olgpumuv9bik03r0s8af4bcth37.apps.googleusercontent.com',
+  // BACKEND_URL: 'http://localhost:8000',
+  BACKEND_URL: 'https://backend-parcial-liart.vercel.app',
 }
